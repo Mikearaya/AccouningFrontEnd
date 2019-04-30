3d930b5806c40a394eb1bae8019fea54bda3424a
Naol <nnale8899@gmail.com>
2019-04-30 11:10:05 +0300

    #918 account catagory service tested

--- src/app/features/account-catagory/account-catagory-api.service.spec.ts
+++ src/app/features/account-catagory/account-catagory-api.service.spec.ts
@@ -1,12 +1,91 @@
-import { TestBed } from '@angular/core/testing';
+import { of, Observable } from "rxjs";
+import { AccountCatagoryApiService } from "./account-catagory-api.service";
+import { AccountCatagory } from "./account-catagory-domain";
 
-import { AccountCatagoryApiService } from './account-catagory-api.service';
+describe("Account catagory api", () => {
+  let accountCatagApi: AccountCatagoryApiService;
+  let httpClient;
+  let accountCatagories: AccountCatagory[];
+  let accountCatagory: AccountCatagory;
 
-describe('AccountCatagoryApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    httpClient = jasmine.createSpyObj(["get", "post", "put", "delete"]);
+    accountCatagApi = new AccountCatagoryApiService(httpClient);
+    accountCatagories = [
+      {
+        Id: 1,
+        CatagoryName: "Catag 1",
+        AccountType: "Asset"
+      },
+      {
+        Id: 2,
+        CatagoryName: "Catag 2",
+        AccountType: "Expense"
+      }
+    ];
+  });
+  describe("Check Account catagory api", () => {
+    it("Should be created", () => {
+      expect(accountCatagApi).toBeTruthy();
+    });
+  });
 
-  it('should be created', () => {
-    const service: AccountCatagoryApiService = TestBed.get(AccountCatagoryApiService);
-    expect(service).toBeTruthy();
+  describe("Get account catagories", () => {
+    let catags;
+    it("Should return account catagories", () => {
+      spyOn(accountCatagApi, "getAccountCatagories").and.returnValue(
+        of(accountCatagories)
+      );
+      accountCatagApi
+        .getAccountCatagories()
+        .subscribe(comps => (catags = comps));
+      expect(catags).toBe(accountCatagories);
+    });
+  });
+
+  describe("Create account catagory", () => {
+    it("Should return single account catagory", () => {
+      const newCatag: AccountCatagory = {
+        Id: 5,
+        CatagoryName: "Account 3",
+        AccountType: "Capital"
+      };
+      httpClient.post.and.returnValue(of(newCatag));
+      accountCatagApi
+        .createAccountCatagory(newCatag)
+        .subscribe((comp: AccountCatagory) => (accountCatagory = comp));
+
+      expect(accountCatagory).toBe(newCatag);
+    });
+  });
+
+  describe("Update account catagory", () => {
+    it("Should return true on success", () => {
+      httpClient.put.and.returnValue(of(true));
+      let updated = false;
+      const updatedComp: AccountCatagory = {
+        Id: 1,
+        CatagoryName: "Catag 1",
+        AccountType: "Revenue"
+      };
+      accountCatagApi
+        .updateAccountCatagory("1", updatedComp)
+        .subscribe(result => (updated = result));
+
+      expect(updated).toBe(true);
+    });
+  });
+
+  // Test AccountsService Deleteaccount Function
+  describe("Delete account catagory", () => {
+    it("Should return true on Success", () => {
+      httpClient.delete.and.returnValue(of(true));
+      let deleted = false;
+      accountCatagApi
+        .deleteAccountCatagory("Catag 1")
+        .subscribe(res => (deleted = res));
+
+      expect(deleted).toBe(true);
+    });
   });
 });
--- src/app/features/account-catagory/account-catagory-api.service.ts
+++ src/app/features/account-catagory/account-catagory-api.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { AccountCatagory } from "./account-catagory-domain";
+import { catchError } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AccountCatagoryApiService {
+  private url = " http://localhost:3000/account-catagory";
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  createAccountCatagory(
+    newAccountCatag: AccountCatagory
+  ): Observable<AccountCatagory> {
+    return this.httpClient
+      .post<AccountCatagory>(`${this.url}`, newAccountCatag)
+      .pipe(catchError(this.handleError));
+  }
+
+  getAccountCatagories(): Observable<AccountCatagory[]> {
+    return this.httpClient.get<AccountCatagory[]>(`${this.url}`);
+  }
+
+  updateAccountCatagory(
+    id: string,
+    updatedAccountCatagory: AccountCatagory
+  ): Observable<boolean> {
+    return this.httpClient
+      .put<boolean>(`${this.url}/${id}`, updatedAccountCatagory)
+      .pipe(catchError(this.handleError));
+  }
+
+  deleteAccountCatagory(id: string): Observable<boolean> {
+    return this.httpClient
+      .delete<boolean>(`${this.url}/${id}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: Response | any) {
+    return Observable.throw(error.status);
+  }
 }
