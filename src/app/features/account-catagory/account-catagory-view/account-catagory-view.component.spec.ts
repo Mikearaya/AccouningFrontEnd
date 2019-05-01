04735aa2be2a0ddf784c85b2a9ee55bbf6f94a41
Naol <nnale8899@gmail.com>
2019-05-01 11:25:14 +0300

    test account catagory view

--- src/app/features/account-catagory/account-catagory-view/account-catagory-view.component.spec.ts
+++ src/app/features/account-catagory/account-catagory-view/account-catagory-view.component.spec.ts
@@ -1,16 +1,22 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { AccountCatagoryViewComponent } from './account-catagory-view.component';
+import { AccountCatagoryViewComponent } from "./account-catagory-view.component";
+import { AccountCatagoryApiService } from "../account-catagory-api.service";
+import { SharedModule } from "src/app/shared/shared.module";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AccountCatagory } from "../account-catagory-domain";
+import { of } from "rxjs";
 
-describe('AccountCatagoryViewComponent', () => {
+describe("AccountCatagoryViewComponent", () => {
   let component: AccountCatagoryViewComponent;
   let fixture: ComponentFixture<AccountCatagoryViewComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AccountCatagoryViewComponent ]
-    })
-    .compileComponents();
+      imports: [SharedModule, RouterTestingModule],
+      declarations: [AccountCatagoryViewComponent],
+      providers: [AccountCatagoryApiService]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +25,35 @@ describe('AccountCatagoryViewComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("Should be created", () => {
     expect(component).toBeTruthy();
+  });
+  describe("Delete catagory", () => {
+    it("Should be called", () => {
+      spyOn(component, "deleteCatagory");
+      component.deleteCatagory("Id");
+      expect(component.deleteCatagory).toHaveBeenCalled();
+    });
+  });
+  describe("ngOnInit", () => {
+    it("Should be called and list all catagories", () => {
+      const response: AccountCatagory[] = [
+        {
+          Id: 1,
+          CatagoryName: "catagory 2",
+          AccountType: "posting type"
+        },
+        {
+          Id: 2,
+          CatagoryName: "catagory 2",
+          AccountType: "posting type"
+        }
+      ];
+      spyOn(component, "loadCatagories").and.returnValue(of(response));
+      component.loadCatagories();
+      fixture.detectChanges();
+      expect(component.loadCatagories).toHaveBeenCalled();
+      expect(response.length).toEqual(2);
+    });
   });
 });
