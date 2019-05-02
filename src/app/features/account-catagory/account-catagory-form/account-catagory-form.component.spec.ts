1dbb8a7d6ce0ee847e41438edd1564279a536645
Naol <nnale8899@gmail.com>
2019-05-02 17:13:17 +0300

    #910 account catagory form creation functionalities tested

--- src/app/features/account-catagory/account-catagory-form/account-catagory-form.component.spec.ts
+++ src/app/features/account-catagory/account-catagory-form/account-catagory-form.component.spec.ts
@@ -1,25 +1,144 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestModuleMetadata,
+  TestBed,
+  async
+} from "@angular/core/testing";
 
-import { AccountCatagoryFormComponent } from './account-catagory-form.component';
+import { DebugElement } from "@angular/core";
 
-describe('AccountCatagoryFormComponent', () => {
+import { SharedModule } from "src/app/shared/shared.module";
+
+import { RouterTestingModule } from "@angular/router/testing";
+
+import { ActivatedRoute, convertToParamMap } from "@angular/router";
+
+import { AccountCatagoryFormComponent } from "./account-catagory-form.component";
+import { AccountCatagoryApiService } from "../account-catagory-api.service";
+
+fdescribe(" Account catagory form component", () => {
   let component: AccountCatagoryFormComponent;
   let fixture: ComponentFixture<AccountCatagoryFormComponent>;
+  let debugEl: DebugElement;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AccountCatagoryFormComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
+  const makeCompiledTestBed = (provider?: object): void => {
+    const moduleDef: TestModuleMetadata = {
+      imports: [SharedModule, RouterTestingModule],
+      declarations: [AccountCatagoryFormComponent],
+      providers: [AccountCatagoryApiService]
+    };
+    if (moduleDef.providers && provider) {
+      moduleDef.providers.push(provider);
+    }
+    TestBed.configureTestingModule(moduleDef).compileComponents();
+  };
+  const setupTestVars = (): void => {
     fixture = TestBed.createComponent(AccountCatagoryFormComponent);
     component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
     fixture.detectChanges();
+  };
+
+  describe("When id is not provided in the url param--(Create account catagory form)", () => {
+    beforeEach(async(makeCompiledTestBed));
+    beforeEach(setupTestVars);
+    it("Should be created ", () => {
+      expect(component).toBeTruthy();
+    });
+
+    it("should check if form have been filled out", () => {
+      TestBed.get(ActivatedRoute);
+      expect(component.CatagoryName.valid).toBeFalsy();
+    });
+
+    describe("OnSubmit", () => {
+      it("Should be called", () => {
+        spyOn(component, "onSubmit");
+        component.onSubmit();
+        expect(component.onSubmit).toHaveBeenCalled();
+      });
+    });
+    describe("Account catagory form", () => {
+      let err = {};
+      const obj1 = {
+        createCatagoryForm: () => null
+      };
+      it("Should be created", () => {
+        spyOn(obj1, "createCatagoryForm");
+        obj1.createCatagoryForm();
+        expect(obj1.createCatagoryForm).toBeTruthy();
+      });
+      const obj2 = {
+        initializeCatagory: () => null
+      };
+      it("should be created for initialize catagory", () => {
+        spyOn(obj2, "initializeCatagory");
+        obj2.initializeCatagory();
+        expect(obj2.initializeCatagory).toBeTruthy();
+      });
+
+      it("Should be invalid when empty", () => {
+        expect(component.catagoryForm.valid).toBeFalsy();
+      });
+      it("Catagory name field validity", () => {
+        const catagoryName = component.CatagoryName;
+        expect(catagoryName.valid).toBeFalsy();
+        catagoryName.setValue("appdiv");
+        expect(catagoryName.valid).toBeTruthy();
+        err = catagoryName.errors || {};
+        expect(err["required"]).toBeFalsy();
+        catagoryName.setValue("");
+        err = catagoryName.errors || {};
+        expect(err["required"]).toBeTruthy();
+      });
+
+      it("Account type field validity", () => {
+        const accountType = component.AccountType;
+        expect(accountType.valid).toBeFalsy();
+        accountType.setValue("appdiv");
+        expect(accountType.valid).toBeTruthy();
+        expect(accountType.value).toEqual("appdiv");
+        err = accountType.errors || {};
+        expect(err["required"]).toBeFalsy();
+        accountType.setValue("");
+        err = accountType.errors || {};
+        expect(err["required"]).toBeTruthy();
+      });
+
+      it("Should be valid when not empty", () => {
+        component.CatagoryName.setValue("Appdiv");
+        component.AccountType.setValue("Appdiv");
+        expect(component.catagoryForm.valid).toBeTruthy();
+      });
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe("When an id is provided in the url param--(Update account catagory form)", () => {
+    beforeEach(async(() => {
+      makeCompiledTestBed({
+        provide: ActivatedRoute,
+        useValue: {
+          snapshot: {
+            paramMap: convertToParamMap({ id: 1 })
+          }
+        }
+      });
+    }));
+    beforeEach(setupTestVars);
+    /* describe("Initialize catagory", () => {
+      let data: any;
+      it("Should be called", () => {
+        spyOn(component, "initializeCatagory");
+        component.initializeCatagory(data);
+        expect(component.initializeCatagory).toHaveBeenCalled();
+      });
+    }); */
+
+    let catagoryId = 1;
+    let accountCatagory;
+    accountCatagory = ActivatedRoute.snapshot.paramMap.get(catagoryId);
+    it("should ..", () => {
+      expect(accountCatagory).toBe(1);
+    });
   });
 });
